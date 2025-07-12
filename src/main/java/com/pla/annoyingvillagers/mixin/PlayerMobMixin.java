8a854c5ce42b00859f0368e4c2139e9681b24848
PlaIsMe <phongvulai96@gmail.com>
2025-07-12 13:34:55 +0700

    Add player mob spawn in surface

--- src/main/java/com/pla/annoyingvillagers/mixin/PlayerMobMixin.java
+++ src/main/java/com/pla/annoyingvillagers/mixin/PlayerMobMixin.java
@@ -1,10 +1,17 @@
 package com.pla.annoyingvillagers.mixin;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.levelgen.Heightmap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,6 +19,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import se.gory_moon.player_mobs.entity.PlayerMobEntity;
 
+import javax.annotation.Nullable;
 import java.util.Random;
 
 @Mixin(value = {PlayerMobEntity.class}, remap = false)
@@ -49,5 +57,19 @@ public class PlayerMobMixin {
     @Inject(method = "setBaby", at = @At("HEAD"), cancellable = true)
     private void blockSetBaby(boolean isChild, CallbackInfo ci) {
         ci.cancel();
+    }
+
+    @Inject(method = "finalizeSpawn", at = @At("RETURN"))
+    private void teleportToSurfaceIfUnderground(ServerLevelAccessor world, DifficultyInstance difficulty, MobSpawnType reason, @Nullable SpawnGroupData spawnData, @Nullable CompoundTag dataTag, CallbackInfoReturnable<SpawnGroupData> cir) {
+        Entity self = (Entity) (Object) this;
+
+        BlockPos pos = self.blockPosition();
+        int currentY = pos.getY();
+        int surfaceY = world.getHeightmapPos(Heightmap.Types.WORLD_SURFACE, pos).getY();
+
+        if (currentY < surfaceY - 5 && world.getRandom().nextFloat() < 0.6F) {
+            BlockPos surfacePos = new BlockPos(pos.getX(), surfaceY, pos.getZ());
+            self.setPos(surfacePos.getX() + 0.5, surfacePos.getY(), surfacePos.getZ() + 0.5);
+        }
     }
 }
