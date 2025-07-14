8244761495ba740a4c4c70fa87237e273014d513
PlaIsMe <phongvulai96@gmail.com>
2025-07-14 10:25:57 +0700

    Add villager scout helmet

--- src/main/java/com/pla/annoyingvillagers/item/ClassicgoldenaItem.java
+++ src/main/java/com/pla/annoyingvillagers/item/ClassicgoldenaItem.java
@@ -1,17 +1,27 @@
 package com.pla.annoyingvillagers.item;
 
 import com.pla.annoyingvillagers.AnnoyingVillagers;
+import com.pla.annoyingvillagers.client.model.ModelScoutH;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item.Properties;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.client.IItemRenderProperties;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Consumer;
 
 public abstract class ClassicgoldenaItem extends ArmorItem {
 
@@ -90,8 +100,21 @@ public abstract class ClassicgoldenaItem extends ArmorItem {
             super(EquipmentSlot.HEAD, (new Properties()).tab(AnnoyingVillagers.ANNOYINGVILLAGERS_TAB));
         }
 
+        public void initializeClient(Consumer<IItemRenderProperties> consumer) {
+            consumer.accept(new IItemRenderProperties() {
+                public HumanoidModel getArmorModel(LivingEntity livingentity, ItemStack itemstack, EquipmentSlot equipmentslot, HumanoidModel humanoidmodel) {
+                    HumanoidModel humanoidmodel1 = new HumanoidModel(new ModelPart(Collections.emptyList(), Map.of("head", (new ModelScoutH<>(Minecraft.getInstance().getEntityModels().bakeLayer(ModelScoutH.LAYER_LOCATION))).Head, "hat", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "body", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "right_arm", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "left_arm", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "right_leg", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "left_leg", new ModelPart(Collections.emptyList(), Collections.emptyMap()))));
+
+                    humanoidmodel1.crouching = livingentity.isShiftKeyDown();
+                    humanoidmodel1.riding = humanoidmodel.riding;
+                    humanoidmodel1.young = livingentity.isBaby();
+                    return humanoidmodel1;
+                }
+            });
+        }
+
         public String getArmorTexture(ItemStack itemstack, Entity entity, EquipmentSlot equipmentslot, String s) {
-            return "annoyingvillagers:textures/models/armor/gold__layer_1.png";
+            return "annoyingvillagers:textures/models/armor/scout_layer_1.png";
         }
     }
 }
