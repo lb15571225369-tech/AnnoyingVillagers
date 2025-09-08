6d971ee951f2deda721fb1e80a26125a0c4951d8
PlaIsMe <97893528+PlaIsMe@users.noreply.github.com>
2025-09-08 10:10:31 +0700

    Fix Null's wither skeleton wrong behavior

--- src/main/java/com/pla/annoyingvillagers/mixin/WitherSkeletonMixin.java
+++ src/main/java/com/pla/annoyingvillagers/mixin/WitherSkeletonMixin.java
@@ -1,15 +1,11 @@
 package com.pla.annoyingvillagers.mixin;
 
-import com.pla.annoyingvillagers.entity.AlexEntity;
 import com.pla.annoyingvillagers.entity.NullEntity;
-import com.pla.annoyingvillagers.util.CommonGoals;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
-import net.minecraft.world.entity.monster.AbstractSkeleton;
-import net.minecraft.world.entity.monster.Skeleton;
 import net.minecraft.world.entity.monster.WitherSkeleton;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,37 +18,62 @@ import java.util.UUID;
 public class WitherSkeletonMixin {
     @Inject(method = "registerGoals", at = @At("HEAD"))
     private void nullWitherSkeleton(CallbackInfo ci) {
-        if ((Object) this instanceof WitherSkeleton witherSkeleton && witherSkeleton.getPersistentData().contains("SpawnByNull")) {
-            UUID nullUUID = witherSkeleton.getPersistentData().getUUID("SpawnByNull");
-            Entity entity = ((ServerLevel) witherSkeleton.level()).getEntity(nullUUID);
-            if (entity instanceof NullEntity nullEntity) {
-                witherSkeleton.goalSelector.addGoal(2, new Goal() {
-                    @Override
-                    public boolean canUse() {
+        WitherSkeleton self = (WitherSkeleton) (Object) this;
+        self.goalSelector.addGoal(2, new Goal() {
+            @Override
+            public boolean canUse() {
+                if (self.getPersistentData().contains("SpawnByNull")) {
+                    UUID nullUUID = self.getPersistentData().getUUID("SpawnByNull");
+                    Entity entity = ((ServerLevel) self.level()).getEntity(nullUUID);
+                    if (entity instanceof NullEntity nullEntity) {
                         return nullEntity != null && nullEntity.isAlive();
                     }
-
-                    @Override
-                    public void tick() {
-                        if (nullEntity != null && nullEntity.isAlive()) {
-                            witherSkeleton.getNavigation().moveTo(nullEntity, 2.0D);
-                        }
-                    }
-
-                    @Override
-                    public boolean canContinueToUse() {
-                        return nullEntity != null && nullEntity.isAlive() && witherSkeleton.distanceTo(nullEntity) > 10.0D;
-                    }
-                });
-                witherSkeleton.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(witherSkeleton, LivingEntity.class, 10, true, false, (target) -> nullEntity != null
-                        && nullEntity.isAlive()
-                        && target != null
-                        && nullEntity.getTarget() == target));
-                witherSkeleton.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(witherSkeleton, LivingEntity.class, 10, true, false, (target) -> nullEntity != null
-                        && nullEntity.isAlive()
-                        && target != null
-                        && target.getLastHurtMob() == nullEntity));
+                }
+                return false;
             }
-        }
+
+            @Override
+            public void tick() {
+                if (self.getPersistentData().contains("SpawnByNull")) {
+                    UUID nullUUID = self.getPersistentData().getUUID("SpawnByNull");
+                    Entity entity = ((ServerLevel) self.level()).getEntity(nullUUID);
+                    if (entity instanceof NullEntity nullEntity) {
+                        self.getNavigation().moveTo(nullEntity, 2.0D);
+                    }
+                }
+            }
+
+            @Override
+            public boolean canContinueToUse() {
+                if (self.getPersistentData().contains("SpawnByNull")) {
+                    UUID nullUUID = self.getPersistentData().getUUID("SpawnByNull");
+                    Entity entity = ((ServerLevel) self.level()).getEntity(nullUUID);
+                    if (entity instanceof NullEntity nullEntity) {
+                        return nullEntity != null && nullEntity.isAlive() && self.distanceTo(nullEntity) > 10.0D;
+                    }
+                }
+                return false;
+            }
+        });
+        self.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(self, LivingEntity.class, 10, true, false, (target) -> {
+            if (self.getPersistentData().contains("SpawnByNull")) {
+                UUID nullUUID = self.getPersistentData().getUUID("SpawnByNull");
+                Entity entity = ((ServerLevel) self.level()).getEntity(nullUUID);
+                if (entity instanceof NullEntity nullEntity) {
+                    return target != null && nullEntity.isAlive() && nullEntity.getTarget() == target;
+                }
+            }
+            return false;
+        }));
+        self.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(self, LivingEntity.class, 10, true, false, (target) -> {
+            if (self.getPersistentData().contains("SpawnByNull")) {
+                UUID nullUUID = self.getPersistentData().getUUID("SpawnByNull");
+                Entity entity = ((ServerLevel) self.level()).getEntity(nullUUID);
+                if (entity instanceof NullEntity nullEntity) {
+                    return target != null && nullEntity.isAlive() && target.getLastHurtMob() == nullEntity;
+                }
+            }
+            return false;
+        }));
     }
 }
